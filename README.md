bcf468d46da9bd8da665cdbefb5ab5d351757bc4
pkzrnvch <pkazarnovich@gmail.com>
2022-05-09 21:44:08 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Devman notification bot
+
+This program uses [Devman](https://dvmn.org) API to check whether any of the submitted tasks have been reviewed, and sends user a notification via a telegram bot.
+
+### How to install
+
+Create an `.env` file in the project directory. Create a new telegram bot through a BotFather and assign its token to `TG_TOKEN` variable. In case you have an account, you can find your token for Devman API [here](https://dvmn.org/api/docs/), assign it to `DEVMAN_TOKEN` variable. Send a message to [@userinfobot](https://t.me/userinfobot) to get your chat_id, assign it to `TG_CHAT_ID` variable. 
+
+Example of an `.env` file:
+
+```
+TG_BOT_TOKEN="YOUR_TOKEN"
+DEVMAN_TOKEN="YOUR_TOKEN"
+TG_CHAT_ID="YOUR_CHAT_ID"
+```
+
+Python3 should already be installed. Use pip (or pip3, in case of conflict with Python2) to install dependencies:
+```
+pip install -r requirements.txt
+```
+
+### Usage
+
+To run the programm use the following command from the project directory:
+```
+python main.py
+```
+
+### Project Goals
+
+The code is written for educational purposes on online-course for web-developers [Devman](https://dvmn.org).
---
+++ main.py
@@ -0,0 +1,75 @@
+import os
+from textwrap import dedent
+from time import sleep
+
+import requests
+import telegram
+from dotenv import load_dotenv
+
+
+def devman_long_polling(devman_token, timestamp):
+    url = 'https://dvmn.org/api/long_polling/'
+    headers = {'Authorization': f'Token {devman_token}'}
+    payload = {}
+    if timestamp:
+        payload = {'timestamp': timestamp}
+    response = requests.get(url, headers=headers, params=payload, timeout=150)
+    response.raise_for_status()
+    return response.json()
+
+
+def form_message(lesson_review):
+    review_successful = not lesson_review['is_negative']
+    lesson_title = lesson_review['lesson_title']
+    lesson_url = lesson_review['lesson_url']
+    if review_successful:
+        message = f"""
+        Ваша работа "{lesson_title}" вернулась с проверки.
+        
+        Преподавателю все понравилось, можно приступать к следующему уроку!
+        {lesson_url}
+        """
+    else:
+        message = f"""
+        Ваша работа "{lesson_title}" вернулась с проверки.
+        
+        К сожалению, к работе возникли замечания. 
+        Перейдите по ссылке, чтобы узнать детали:
+        {lesson_url}
+        """
+    return dedent(message)
+
+
+def main():
+    load_dotenv()
+    devman_token = os.environ.get('DEVMAN_TOKEN')
+    tg_bot_token = os.environ.get('TG_BOT_TOKEN')
+    chat_id = os.environ.get('TG_CHAT_ID')
+    bot = telegram.Bot(token=tg_bot_token)
+    timestamp = 0
+    while True:
+        try:
+            response = devman_long_polling(devman_token, timestamp)
+        except requests.HTTPError:
+            sleep(10)
+            continue
+        except requests.ReadTimeout:
+            print('readtime')
+            continue
+        except requests.ConnectionError:
+            print('connect.err')
+            sleep(60)
+            continue
+        if response['status'] == 'found':
+            print(response)
+            lesson_review = response['new_attempts'][0]
+            message = form_message(lesson_review)
+            bot.send_message(text=message, chat_id=chat_id)
+            timestamp = response['last_attempt_timestamp']
+        elif response['status'] == 'timeout':
+            print(response)
+            timestamp = response['timestamp_to_request']
+
+
+if __name__ == '__main__':
+    main()
