bd22f8d7d2742007d57bfea731ac0b628547f7d9
pkzrnvch <pkazarnovich@gmail.com>
2022-05-20 00:06:56 +0500

    Send logs to Telegram to admin user

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import os
+import logging
 from textwrap import dedent
 from time import sleep
 
 import requests
 import telegram
 from dotenv import load_dotenv
+
+
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def form_message(lesson_review):
@@ -35,22 +48,37 @@ def main():
     tg_bot_token = os.environ.get('TG_BOT_TOKEN')
     chat_id = os.environ.get('TG_CHAT_ID')
     bot = telegram.Bot(token=tg_bot_token)
+    logger = logging.getLogger('Logger')
+    logger.setLevel(logging.INFO)
+    logger.addHandler(TelegramLogsHandler(bot, chat_id))
+    logger.info('Бот запущен!')
     timestamp = None
     url = 'https://dvmn.org/api/long_polling/'
     headers = {'Authorization': f'Token {devman_token}'}
     while True:
         try:
             payload = {'timestamp': timestamp}
-            response = requests.get(url, headers=headers, params=payload, timeout=150)
+            response = requests.get(
+                url,
+                headers=headers,
+                params=payload,
+                timeout=150
+            )
             response.raise_for_status()
             review = response.json()
         except requests.HTTPError:
-            sleep(10)
+            logger.error('Ошибка HTTP, попытка переподключения через 5 минут')
+            sleep(300)
             continue
         except requests.ReadTimeout:
+            logger.warning('Нет ответа от сервера, попытка переподключения...')
             continue
         except requests.ConnectionError:
+            logger.warning('Потеря связи, попытка переподключения...')
             sleep(60)
+            continue
+        except Exception:
+            logger.exception('Бот cтолкнулся с непредвиденной ошибкой:')
             continue
         if review['status'] == 'found':
             review_details = review['new_attempts'][0]
